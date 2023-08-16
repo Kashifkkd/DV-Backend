3b2c5895c0fa943c83a0c2b19eace854dfd8ccab
Kashif Deshmukh <kashifdeshmukh@Kashifs-MacBook-Air.local>
2023-08-16 20:52:45 +0530

    done

--- server.js
+++ server.js
@@ -3,11 +3,19 @@ const mongoose  = require('mongoose');
 const dotenv = require('dotenv');
 const pathMiddleware = require('./middlewares/pathMiddleware');
 const dataRoutes = require('./routes/dataRoutes')
+const cors = require('cors');
 
 const app = express();
 dotenv.config()
 
 const PORT = process.env.PORT || 4000
+
+app.use(cors
+    ({
+        origin : "https://capable-druid-72a601.netlify.app",
+        methods: ["GET", "POST", "PUT", "PATCH", "DELETE"]
+    })
+)
 
 app.use(express.json())
 app.use(express.static('public'));
