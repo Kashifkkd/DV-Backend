fcaf338fd2224bd7d956f3e73fc1f7b6605cb5a3
Kashif Deshmukh <kashifdeshmukh@Kashifs-MacBook-Air.local>
2023-08-16 20:59:15 +0530

    added vercel link

--- server.js
+++ server.js
@@ -12,7 +12,7 @@ const PORT = process.env.PORT || 4000
 
 app.use(cors
     ({
-        origin : "https://capable-druid-72a601.netlify.app",
+        origin : "https://dv-frontend.vercel.app",
         methods: ["GET", "POST", "PUT", "PATCH", "DELETE"]
     })
 )
