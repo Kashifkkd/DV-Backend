42b14c683aa8eccf82491077d543a66d236218e4
Kashif Deshmukh <kashifdeshmukh@Kashifs-MacBook-Air.local>
2023-08-16 18:37:29 +0530

    first commit

---
+++ server.js
@@ -0,0 +1,30 @@
+const express = require('express');
+const mongoose  = require('mongoose');
+const dotenv = require('dotenv');
+const pathMiddleware = require('./middlewares/pathMiddleware');
+const dataRoutes = require('./routes/dataRoutes')
+
+const app = express();
+dotenv.config()
+
+const PORT = process.env.PORT || 4000
+
+app.use(express.json())
+app.use(express.static('public'));
+app.use(pathMiddleware)
+
+app.get('/', (req, res) => {
+    res.send('Hello world')
+})
+
+app.use('/api',dataRoutes)
+
+mongoose.connect(process.env.MONGODB_URI)
+    .then(() => {
+        app.listen(PORT, () => {
+            console.log(`Connected to db & listening on port 4000`);
+        })
+    })
+    .catch((err) => {
+        console.log(error)
+    })
